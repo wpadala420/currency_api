f95c79f27165360aa84df3faf62f0a6f2eddbf5e
Wojciech Padała <wojciech.padala@skanuj.to>
2021-02-04 15:36:05 +0100

    create simple subscription model

--- currency_api/api/models.py
+++ currency_api/api/models.py
@@ -4,5 +4,9 @@ class User(models.Model):
     name = models.CharField(max_length=10)
     email = models.CharField(max_length=100)
 
+class CurrencySubscription(models.Model):
+    subscribent = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.DateTimeField(auto_now_add=True)
+    currency_name = models.CharField(max_length=20)
 
 # Create your models here.
